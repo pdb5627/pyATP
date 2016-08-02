81caeb8f25b23544dc17ef4e905a97679fed4775
Paul Brown <pdb.lists@gmail.com>
2016-08-02 07:15:58 -0500

    Begin factoring text_data_cards to separate package. Not working in development environment. It may be a problem with how the text_data_cards package is set up (__init__.py).

--- pyATP.py
+++ pyATP.py
@@ -16,14 +16,10 @@ try:
 except ImportError:
     pass
     
-# To parse ATP files, the fortranformat module is used
-# Install from pip: pip install fortramformat
-#try:
-from fortranformat import FortranRecordReader
-from fortranformat import FortranRecordWriter
-#from fortranformat import RecordError
-#except ImportError:
-#    pass
+# To parse ATP files, the test_data_cards module is used.
+# It is currently under development.
+import text_data_cards as tdc
+
 import itertools, copy, os
 
 import subprocess, re, csv, codecs, shutil
@@ -329,165 +325,6 @@ def extract_ABCD(ATP_template, ATP_tmp, current_key, switch_key,
     
     return ABCD
 
-class DataCard:
-    ''' Class to implement a line of generalized ATP/Fortran style input records.
-        format is a format string suitable for the fortranformat module.
-        fields is a list of field names for indexing the data dict. Field names
-            will usually be strings, but could be integers or floats in the case
-            case of matching fixed_fields.
-        fixed_fields is an iterable of field indices that have fixed values.
-            The expected value for the field should be the field name in the
-            fields  list.
-        Data in the line is internally represented using a dict.
-                
-        format and fields should not be changed after initialization.
-        
-        Reads only one line, but should be passed an interable of lines.
-    '''
-    def __init__(self, format, fields, fixed_fields=(), name=None):
-        self._fields = fields
-        self._fixed_fields = tuple(fixed_fields)
-        self._reader = FortranRecordReader(format)
-        self._writer = FortranRecordWriter(format)
-        self.name = name
-        
-        self.data = {}
-        for f in fields:
-            if f is not None:
-                self.data[f] = None
-        
-    def read(self, lines):
-        ''' Read in datalines with validation prior to populating data. '''
-        if not self.match(lines):
-            # This should raise an exception and will help
-            # identify where in the stack the exception occured.
-            tmp = copy.deepcopy(self)
-            tmp._read(lines)
-        self._read(lines)
-
-    def _read(self, lines):
-        line = lines[0]
-        data = self._reader.read(line)
-        for f in self._fixed_fields:
-            if data[f] != self._fields[f]:
-                raise ValueError('Fixed field with wrong value: '+data[f]+'/'+self._fields[f])
-
-        for f, d in zip(self._fields, data):
-            if f is not None:
-                self.data[f] = d
-        
-        return self
-    
-    def write(self):
-        data = [self.data[f] if f is not None else None for f in self._fields]
-        return self._writer.write(data)
-        
-    def match(self, lines):
-        ''' Checks if text lines match record type. Does not modify card data.
-        '''
-        tmp = copy.deepcopy(self)
-        try:
-            tmp._read(lines)
-        except ValueError:
-            return False
-        
-        return True
-        
-    def num_lines(self):
-        return 1
-
-class DataCardFixedText(DataCard):
-    def __init__(self, text, name=None):
-        DataCard.__init__(self, format='(A%d)' % len(text),
-              fields=[text], fixed_fields=(0,), name=None)
-            
-class DataCardStack(DataCard):
-    ''' Class to implement generalized ATP/Fortran style input records.
-        datalines is a list of DataLine objects. It represents a single data card
-        that spans multiple lines. Fields must be unique among all lines.
-        
-        data is represented internally using a dict.
-        
-        TODO: List of cards with termination card. Different class??
-    '''
-    def __init__(self, datalines, name=None):
-        self._datalines = copy.deepcopy(datalines)
-        self.name = name
-        self._fixed_fields = ()
-        
-        self.data = {}
-        for dl in self._datalines:
-            for f in dl._fields:
-                self.data[f] = None
-    
-    def _read(self, lines):
-        ''' Read in datalines with no validation. Throw ValueError if records
-            don't match up.
-        '''
-        line_idx = 0
-        for dl in self._datalines:
-            dl._read(lines[line_idx:])
-            line_idx += dl.num_lines()
-            # Sync data up to DataCardFixed.data dict.
-            for f in dl._fields:
-                self.data[f] = dl.data[f]
-            if dl.name is not None:
-                self.data[dl.name] = dl.data
-                
-        return self
-                
-    def write(self):
-        rtn = []
-        for dl in self._datalines:
-            # Sync data down to DataLine.data dicts.
-            for f in dl._fields:
-                dl.data[f] = dl.data[f]
-            rtn.append(dl.write())
-        return '\n'.join(rtn)
-        
-    def num_lines(self):
-        return sum([dl.num_lines() for dl in self._datalines])
-        
-class DataCardRepeat(DataCardStack):
-    ''' Class to implement ATP/Fortram style input records where a record
-        may be repeated some number of times. Records are read until an
-        end-marker record is found.
-        
-        For now, repeated_record and end_record have to be a single-line records.
-        That restriction should change in the future, but it will probably
-        require changing the interface to the other classes to an iterator or
-        similar.
-        
-        Data is stored as a list of cards. Access by index or iteration only at
-        this time.
-    '''
-    def __init__(self, repeated_record, end_record, name=None):
-        self._repeated_record = copy.deepcopy(repeated_record)
-        self.end_record = copy.deepcopy(end_record)
-        self._datalines = []
-        self.data = []
-        self.name = name
-        self._fields = []
-        
-    def _read(self, lines):
-
-        self.data = []
-        # Loop breaks internally due to complexity of break conditions
-        line_idx = 0
-        while line_idx < len(lines):
-            if self.end_record.match(lines[line_idx:]):
-                self._datalines.append(self.end_record)
-                self.end_record._read(lines[line_idx:])
-                line_idx += self.end_record.num_lines()
-                break
-            # Read record and append to records list
-            r = copy.deepcopy(self._repeated_record)
-            self._datalines.append(r)
-            r._read(lines[line_idx:])
-            line_idx += r.num_lines()
-            self.data.append(r.data)
-
-        return self
     
 # Parsing function for reading / modifying line constant cards. See Rule Book
 # Chapter 21. Below is an example of such a card for a three-phase line:
@@ -510,24 +347,25 @@ BEGIN NEW DATA CASE
 BLANK CARD
 '''
 
-class LineConstCards(DataCardStack):
+class LineConstCards(tdc.DataCardStack):
     ''' Stack of cards for a line constants case.
         This is Based on what ATPDraw creates.'''
     def __init__(self):
-        conductor_card = DataCard('(I3, F5.4, F8.5, I2, F8.5, F8.5, F8.3, F8.3, F8.3)',
+        conductor_card = tdc.DataCard('(I3, F5.4, F8.5, I2, F8.5, F8.5, F8.3, F8.3, F8.3)',
                                  ['IP', 'SKIN', 'RESIS', 'IX', 'REACT', 'DIAM', 'HORIZ', 'VTOWER', 'VMID'])
-        end_conductors = DataCardFixedText('BLANK CARD ENDING CONDUCTOR CARDS')
-        conductor_cards = DataCardRepeat(conductor_card, end_record = end_conductors,
+        end_conductors = tdc.DataCardFixedText('BLANK CARD ENDING CONDUCTOR CARDS')
+        conductor_cards = tdc.DataCardRepeat(conductor_card, end_record = end_conductors,
                                          name = 'conductors')
 
-        DataCardStack.__init__(self, [
-                        DataCardFixedText('BEGIN NEW DATA CASE'),
-                        DataCardFixedText('LINE CONSTANTS'),
-                        DataCardFixedText('$ERASE'),
-                        DataCard('(A8,6A6)', ['branch_card', 'in1', 'out1', 'in2', 'out2', 'in3', 'out3']),
-                        DataCard('(A80)', ['units']),
+        tdc.DataCardStack.__init__(self,
+            [tdc.DataCardFixedText('BEGIN NEW DATA CASE'),
+             tdc.DataCardFixedText('LINE CONSTANTS'),
+             tdc.DataCardFixedText('$ERASE'),
+             tdc.DataCard('(A8,6A6)', ['branch_card', 'in1', 'out1', 'in2',
+                                       'out2', 'in3', 'out3']),
+             tdc.DataCard('(A80)', ['units']),
                         conductor_cards,
-                        DataCard('(F8.2, F10.2, A10, A1, 6I1, A1, 6I1, A1, I1, F8.3, A1, 4I1, I1, A7, I3)',
+             tdc.DataCard('(F8.2, F10.2, A10, A1, 6I1, A1, 6I1, A1, I1, F8.3, A1, 4I1, I1, A7, I3)',
                                        ['RHO', 'FREQ', 'FCAR', '_1',
                                         'inv_C', 'inv_Ce', 'inv_Cs',
                                         'C', 'Ce', 'Cs', '_2',
@@ -538,12 +376,12 @@ class LineConstCards(DataCardStack):
                                         'pi_Y', 'pi_Ys', 'pi_Z', 'pi_Zs',
                                         'ISEG', '_5',
                                         'PUN']),
-                        DataCardFixedText('$PUNCH'),
-                        DataCardFixedText('BLANK CARD ENDING FREQUENCY CARDS'),
-                        DataCardFixedText('BLANK CARD ENDING LINE CONSTANT'),
-                        DataCardFixedText('BEGIN NEW DATA CASE'),
-                        DataCardFixedText('BLANK CARD')
-                    ])
+             tdc.DataCardFixedText('$PUNCH'),
+             tdc.DataCardFixedText('BLANK CARD ENDING FREQUENCY CARDS'),
+             tdc.DataCardFixedText('BLANK CARD ENDING LINE CONSTANT'),
+             tdc.DataCardFixedText('BEGIN NEW DATA CASE'),
+             tdc.DataCardFixedText('BLANK CARD')
+            ])
 
 # Quick and dirty hack to build lib files. Will only work for three-phase lines.
 ATPline_lib_head = ['''KARD  4  4  5  5  7  7
