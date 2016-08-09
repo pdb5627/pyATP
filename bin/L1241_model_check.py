7f666c1568f31e1f2655b39dfd6c5c183158f526
pdb5627 <pdb.lists@gmail.com>
2016-08-08 21:16:51 -0500

    Still working on it

--- bin/L1241_model_check.py
+++ bin/L1241_model_check.py
@@ -53,14 +53,19 @@ parser.add_argument('binary_met_data',
 parser.add_argument('atp_pch_folder',
                     help='Folder in which ATP pch files of line parameters '
                          'are found.')
+parser.add_argument('binary_atp_data',
+                    type=argparse.FileType('rb'),
+                    help='Name of binary (Python pickle format) '
+                         'file to read. See make_ss_csv.py.')
 
 
-def terminal_states(line_defs, line):
+def terminal_state(line_defs, line, terminal=(0, 1)):
     """ Extract phase voltages and currents from meter data in the line_defs
         dict.
     """
     rtn = []
-    for s in ('term0_met', 'term1_met'):
+    for t in terminal:
+        s = 'term%d_met' % t
         t = line_defs[line][s]
         V_ph = np.array(
             [t['V%s' % ph] for ph in ['A', 'B', 'C']]) * 1e3  # kV to V
@@ -77,7 +82,7 @@ def terminal_states(line_defs, line):
 def set_angles(line_defs, bus, buses_set=None):
     # On first call, set the bus to be reference
     if buses_set is None:
-        buses_set = {bus: 14.4}
+        buses_set = {bus: 12.35}
 
     for line, l in line_defs.items():
         if bus == l['terminals'][0]:
@@ -94,15 +99,12 @@ def set_angles(line_defs, bus, buses_set=None):
 
         # Calculate remote bus angle from three-phase power and
         # positive-sequence  line parameters.
-        #if 'V1_MAG' in l['term%d_met' % term]:
         try:
             V1_mag = 1000. * l['term%d_met' % term]['V1_MAG']
         except KeyError:
             try:
-                #elif 'VA_MAG' in l['term%d_met' % term]:
                 V1_mag = 1000. * l['term%d_met' % term]['VA_MAG']
             except KeyError:
-                #else:
                 V1_mag = 115e3 / math.sqrt(3)
         try:
             P = l['term%d_met' % term]['MW_3P']
@@ -129,32 +131,62 @@ def main(argv=None):
     with args.binary_met_data as picklefile:
         data_list = pickle.load(picklefile)
 
+    # Read ATP steady-state data from pickle file.
+    with args.binary_atp_data as picklefile:
+        atp_data_list = pickle.load(picklefile)
+
     line_defs = {'L1078B': {'terminals': ('Thedford', 'Stapleton'),
-                            'atp_segs': ('L78BB', 'L78BA')},
+                            'atp_segs': ('L78BB', 'L78BA'),
+                            'atp_branches': (('THEDF', 'THSTA'),
+                                             ('STAPL', 'STATH'))},
                  'L1078A': {'terminals': ('Stapleton', 'Maxwell'),
-                            'atp_segs': ('L78AB', 'L78AA')},
+                            'atp_segs': ('L78AB', 'L78AA'),
+                            'atp_branches': (('STAPL', 'STMAX'),
+                                             ('MAXWE', 'MAXST'))},
                  'L1140A': {'terminals': ('N. Platte', 'Maxwell'),
-                            'atp_segs': ('L40AA', 'L40AB', 'L40AC')},
+                            'atp_segs': ('L40AA', 'L40AB', 'L40AC'),
+                            'atp_branches': (('NPLAT', 'NPMAX'),
+                                             ('MAXWE', 'MAXNP'))},
                  'L1140B': {'terminals': ('Maxwell', 'Callaway'),
-                            'atp_segs': ('L140B',)},
+                            'atp_segs': ('L140B',),
+                            'atp_branches': (('MAXWE', 'MAXCA'),
+                                             ('CALLA', 'CAMAX'))},
                  'L1140C': {'terminals': ('Callaway', 'Broken Bow'),
-                            'atp_segs': ('L140C',)},
+                            'atp_segs': ('L140C',),
+                            'atp_branches': (('CALLA', 'CALBB'),
+                                             ('BROKE', 'BBCAL'))},
                  'L1240A': {'terminals': ('Broken Bow', 'BB Wind'),
-                            'atp_segs': ('L40A1', 'L40A2', 'L40A3', 'L240B')},
+                            'atp_segs': ('L40A1', 'L40A2', 'L40A3', 'L240B'),
+                            'atp_branches': (('BROKE', 'BBMCK'),
+                                             ('BBWIN', 'BBWMC'))},
                  'L1170':  {'terminals': ('Broken Bow', 'Loup City'),
-                            'atp_segs': ('L170A', 'L170B')},
+                            'atp_segs': ('L170A', 'L170B'),
+                            'atp_branches': (('BROKE', 'BBLC'),
+                                             ('LOUPC', 'LCBB'))},
                  'L1074':  {'terminals': ('Broken Bow', 'Crooked Creek'),
-                            'atp_segs': ('L1074',)},
+                            'atp_segs': ('L1074',),
+                            'atp_branches': (('BROKE', 'BBCCK'),
+                                             ('CROOK', 'CCKBB'))},
                  'L1269':  {'terminals': ('Loup City', 'St. Libory'),
-                            'atp_segs': ('L1269',)},
+                            'atp_segs': ('L1269',),
+                            'atp_branches': (('LOUPC', 'LCSTL'),
+                                             ('STLIB', 'STLLC'))},
                  'L1188':  {'terminals': ('Loup City', 'N. Loup'),
-                            'atp_segs': ('L1188',)},
+                            'atp_segs': ('L1188',),
+                            'atp_branches': (('LOUPC', 'LCNL'),
+                                             ('NLOUP', 'NLLC'))},
                  'L1088':  {'terminals': ('N. Loup', 'Ord'),
-                            'atp_segs': ('L1088',)},
+                            'atp_segs': ('L1088',),
+                            'atp_branches': (('NLOUP', 'NLORD'),
+                                             ('ORD', 'ORDNL'))},
                  'L1192A': {'terminals': ('N. Loup', 'Spalding'),
-                            'atp_segs': ('L192A',)},
+                            'atp_segs': ('L192A',),
+                            'atp_branches': (('NLOUP', 'NLSPA'),
+                                             ('SPALD', 'SPANL'))},
                  'L1192B': {'terminals': ('Spalding', 'Albion'),
-                            'atp_segs': ('L192B',)}}
+                            'atp_segs': ('L192B',),
+                            'atp_branches': (('SPALD', 'SPAAL'),
+                                             ('ALBIO', 'ALSPA'))}}
 
     # Save meter data to lookup dict keyed on location and terminal to make
     # it easy to access data. The last data found for any given location and
@@ -180,6 +212,12 @@ def main(argv=None):
                 params = pyATP.LineConstPCHCards()
                 params.read(pch_lines)
                 line_defs[line]['atp_params'].append(params)
+
+    # Pull in ATP steady-state results gathered by make_ss_csv
+    for line, l in line_defs.items():
+        for terminal, branch in zip(l['terminals'], l['atp_branches']):
+            l['term%d_atp_branch' % idx] = atp_data_list['branch_data'][branch]
+            l['term%d_atp_bus' % idx] = atp_data_list['bus_data'][branch[0]]
 
     # Combine segment ABCD matrices into ABCD of line in phase and sequence
     # components
@@ -219,64 +257,14 @@ def main(argv=None):
                       l['Zsum_s'][0, 0],
                       Polar(Z_s[2, 1])))
 
-        # for p in itertools.permutations((0, 1, 2)):
-        #     print('{}, {:.4f}'.format(p, Polar(lineZ.ph_to_seq_m(\
-        #         lineZ.apply_phasing(Z, p))[2, 1])))
-
-        # plot_data = lineZ.ph_to_seq_m(T) - np.eye(6)
-        # print(plot_data)
-        # column_labels = ['V1_A', 'V1_B', 'V1_C', 'I1_A', 'I1_B', 'I1_C']
-        # row_labels = ['V2_A', 'V2_B', 'V2_C', 'I2_A', 'I2_B', 'I2_C']
-        # fig, ax = plt.subplots()
-        # heatmap = ax.pcolor(plot_data, cmap=plt.cm.Blues)
-        # ax.set_xticks(np.arange(plot_data.shape[0]) + 0.5, minor=False)
-        # ax.set_yticks(np.arange(plot_data.shape[1]) + 0.5, minor=False)
-        # ax.invert_yaxis()
-        # ax.xaxis.tick_top()
-        # ax.set_xticklabels(row_labels, minor=False)
-        # ax.set_yticklabels(column_labels, minor=False)
-        # plt.show()
-
-        # pf = 0.1 # Pos-sequence power factor
-        # S1_out = np.linspace(0., 120.e6)
-        # V1 = np.broadcast_to(np.array([[1.], [68.5e3], [350.]]), (3, 50))
-        # S2_out = np.broadcast_to(3500.j, S1_out.shape)
-        # S0_out = np.broadcast_to(0., S1_out.shape)
-        # S_out = np.stack((S0_out, S1_out*(pf - 1j*np.sqrt(1 - pf**2)), S2_out))
-        # I1 = np.conj(S_out/V1/3)
-        # X1 = np.concatenate((V1, I1), axis=0)
-        # X2 = T_s.dot(X1)
-        # V2 = X2[:3, :]
-        # I2 = X2[3:, :]
-        # S_in = 3*V2*np.conj(I2)
-        # delta_S = S_in - S_out
-        # delta_P = delta_S.real
-        # delta_Q = delta_S.imag
-        # delta_V = np.absolute(V2) - np.absolute(V1)
-        # plt.subplot(2, 1, 1)
-        # plt.title('Positive-sequence Line Losses')
-        # plt.plot(S1_out / 1e6, delta_P[1, :]/1e6, label='P')
-        # plt.plot(S1_out / 1e6, delta_Q[1, :]/1e6, label='Q')
-        # plt.plot(S1_out / 1e6, delta_V[1, :] / 1e3, label='V')
-        # plt.legend()
-        # plt.subplot(2, 1, 2)
-        # plt.title('Negative-sequence Line Losses')
-        # plt.plot(S1_out / 1e6, delta_P[2, :], label='P')
-        # plt.plot(S1_out / 1e6, delta_Q[2, :], label='Q')
-        # plt.plot(S1_out / 1e6, delta_V[2, :], label='V')
-        # plt.legend()
-        # plt.show()
-        # print(S_in - S_out)
-        # break
-
         try:
-            X1_ph_m, X2_ph_m = terminal_states(line_defs, line)
+            X1_ph_m, X2_ph_m = terminal_state(line_defs, line)
         except KeyError:
             continue
         print(line)
 
         # Shift measured phasors by calculated angle relative to ref. bus
-        X1_ph_m *= cmath.exp(1j*math.radians(bus_angles[bus0]))
+        X1_ph_m *= cmath.exp(1j * math.radians(bus_angles[bus0]))
         X2_ph_m *= cmath.exp(1j * math.radians(bus_angles[bus1]))
 
         # Calculate symmetrical components
@@ -318,17 +306,6 @@ def main(argv=None):
                   .format(Polar(X1_s_m[4] * Z_s[2, 1])))
             print('Total V2 Voltage Drop: {:7.2f}'
                   .format(Polar(X1_s_m[5] * Z_s[2, 2] + X1_s_m[4] * Z_s[2, 1])))
-
-
-        # print()
-        # X1_seq = lineZ.ph_to_seq_v(X1_ph)
-        # T_seq = lineZ.ph_to_seq_m(T)
-        # X2_seq = T_seq.dot(X1_seq)
-        # print(X1_seq)
-        # print(X2_seq)
-        # print(np.angle(X1_seq, deg=True))
-        # print(np.angle(X2_seq, deg=True))
-        # print(np.angle(X2_seq, deg=True) - np.angle(X1_seq, deg=True))
 
     print("--- Completed in %s seconds ---" % (time_fun() - start_time))
 
